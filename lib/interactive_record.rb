be7ae5ef4942daef158aec39ad1fabc659bb6c44
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-30 03:13:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,4 +55,14 @@ def self.column_names
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
+  
+   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{table_name}
+      WHERE name = ?
+      SQL
+
+    DB[:conn].execute(sql, name);
+  end
+  
 end
